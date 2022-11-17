b79019f5f35b64ff88a46d116cd39b1d35193698
intersoom <sallybig@naver.com>
2022-11-18 07:23:27 +0900

    코드푸쉬 설정 / 아이콘 설ㅓ

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import RootStack from './src/screens/RootStack';
 import {Pressable, SafeAreaView, StyleSheet} from 'react-native';
 import {RecoilRoot} from 'recoil';
 import SplashScreen from 'react-native-splash-screen';
-
+import CodePush from 'react-native-code-push';
 import {QueryClientProvider, QueryClient} from 'react-query';
 
 function App() {
@@ -32,6 +32,17 @@ function App() {
   );
 }
 
+const codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  // 언제 업데이트를 체크하고 반영할지를 정한다.
+  // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
+  // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
+  updateDialog: false,
+  // 업데이트를 할지 안할지 여부에 대한 노출 (잠수함 패치의 경우 false)
+  installMode: CodePush.InstallMode.IMMEDIATE,
+  // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
+};
+
 const styles = StyleSheet.create({});
 
-export default App;
+export default CodePush(codePushOptions)(App);
